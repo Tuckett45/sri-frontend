aa9a93e6fefe8d9506ccc1c6bff4d78aff5b5d9d
Tuckett45 <tucausti@icloud.com>
2024-09-26 13:30:47 -0500

    Adding Charts.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReportComponent } from './report/report.component';
+import { Chart } from 'chart.js';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    Chart
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
