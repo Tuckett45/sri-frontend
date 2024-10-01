c4d1c4363559ab4a6d553cca764ab756c5b44f39
Tuckett45 <tucausti@icloud.com>
2024-10-01 17:17:04 -0500

    Latest. Table gets populated by modal successfully

--- src/app/components/preliminary-punch-list/preliminary-punch-list.component.ts
+++ src/app/components/preliminary-punch-list/preliminary-punch-list.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PreliminaryPunchListModalComponent } from './preliminary-punch-list-modal.component';
 import { PreliminaryPunchList } from 'src/app/models/preliminary-punch-list.model';
-
+import { PreliminaryPunchListService } from 'src/app/services/preliminary-punch-list.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-preliminary-punch-list',
   templateUrl: './preliminary-punch-list.component.html',
   styleUrls: ['./preliminary-punch-list.component.scss']
 })
-export class PreliminaryPunchListComponent {
-  // Array to store issue reports
-  preliminaryPunchList: PreliminaryPunchList[] = [];
+export class PreliminaryPunchListComponent implements OnInit {
+  // Observable for punch list entries
+  preliminaryPunchList$: Observable<PreliminaryPunchList[]>;
 
   // Table column headers
-  displayedColumns: string[] = ['segmentId', 'streetAddress', 'city', 'state', 'vaultIssues', 'dbIssues', 'trenchIssues', 'siteCleanUp', 'sidewalkPanels', 'sealantIssues', 'additionalConcerns', 'notifiedTo', 'notifiedHow', 'dateReported', 'issueImage', 'pmResolved', 'cmResolved', 'resolvedDate', 'actions'];
+  displayedColumns: string[] = [
+    'segmentId', 'streetAddress', 'city', 'state', 'vaultIssues', 'dbIssues',
+    'trenchIssues', 'siteCleanUp', 'sidewalkPanels', 'sealantIssues',
+    'additionalConcerns', 'notifiedTo', 'notifiedHow', 'dateReported',
+    'issueImage', 'pmResolved', 'cmResolved', 'actions'
+  ];
 
-  constructor(private dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private punchListService: PreliminaryPunchListService
+  ) {
+    this.preliminaryPunchList$ = this.punchListService.getEntries();
+  }
+
+  ngOnInit(): void {}
 
   // Open modal to add a new entry
   openModal(): void {
@@ -25,16 +38,21 @@ export class PreliminaryPunchListComponent {
       data: {}
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result: PreliminaryPunchList | undefined) => {
       if (result) {
-        // Add the result (new issue report) to the preliminaryPunchList array
-        this.preliminaryPunchList.push(result);
+        // Add the result to the shared punch list service
+        this.punchListService.addEntry(result);
       }
     });
   }
 
-  // Method to remove a report (for simplicity)
+  // Method to remove a report
   removeReport(index: number): void {
-    this.preliminaryPunchList.splice(index, 1);
+    this.punchListService.removeEntry(index);
+  }
+
+  // Convert image file to object URL for display in the table
+  getImageUrl(file: File): string {
+    return URL.createObjectURL(file);
   }
 }
---
+++ src/app/services/preliminary-punch-list.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { PreliminaryPunchList } from 'src/app/models/preliminary-punch-list.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PreliminaryPunchListService {
+  // Use a BehaviorSubject to keep the list of entries and allow components to subscribe to changes
+  private punchListEntriesSubject: BehaviorSubject<PreliminaryPunchList[]> = new BehaviorSubject<PreliminaryPunchList[]>([]);
+  
+  constructor() {}
+
+  // Get the current value of the entries
+  getEntries(): Observable<PreliminaryPunchList[]> {
+    return this.punchListEntriesSubject.asObservable();
+  }
+
+  // Add a new entry to the list
+  addEntry(entry: PreliminaryPunchList): void {
+    const currentEntries = this.punchListEntriesSubject.value;
+    this.punchListEntriesSubject.next([...currentEntries, entry]);
+  }
+
+  // Remove an entry by index
+  removeEntry(index: number): void {
+    const currentEntries = [...this.punchListEntriesSubject.value];
+    currentEntries.splice(index, 1);
+    this.punchListEntriesSubject.next(currentEntries);
+  }
+}
