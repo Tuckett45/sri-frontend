1c95e7fced2fde3eccffa93008333ce57c64502b
Tuckett45 <tucausti@icloud.com>
2024-10-28 11:23:15 -0500

    Latest

--- src/auth.guard.spec.ts
+++ src/auth.guard.spec.ts
@@ -1,17 +1,20 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { CanActivate } from '@angular/router';
 
-import { authGuard } from './auth.guard';
-
-describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AuthGuard] // Provide the AuthGuard for dependency injection
+    });
+    guard = TestBed.inject(AuthGuard); // Inject the AuthGuard instance
   });
 
   it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+    expect(guard).toBeTruthy(); // Check that the guard instance is created
   });
+
+  // Add additional tests to check guard functionality
 });
