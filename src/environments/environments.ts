fc4715d7460103b1ca3caaa2cc0c3d670fbffe9e
Tuckett45 <tucausti@icloud.com>
2024-10-29 09:21:38 -0500

    environment

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://sri-backend.azure-api.net/data-api',
+    apiUrl: 'https://sri-backend.azure-api.net/api',
     apiSubscriptionKey: 'ffd675634ab645d7845640bb88d672d8'
   };
