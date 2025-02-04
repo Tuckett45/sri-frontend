71ce5ac6022d54b5dfae269ba7f2f4e05e49a5c5
Tuckett45 <tucausti@icloud.com>
2025-02-04 16:10:33 -0600

    environment file

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,7 @@ app.use((req, res, next) => {
   const allowedOrigins = ['http://localhost:4200', 'https://ark-sri.com']; // Add any other origins here as needed
   const origin = req.headers.origin;
 
-  if (allowedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin); // Allow the origin dynamically
-  }
-
+  res.header('Access-Control-Allow-Origin', 'https://www.ark-sri.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
