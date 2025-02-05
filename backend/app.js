f2af4d22bd451e0c8f21b538d8997c0302d93c2e
Tuckett45 <tucausti@icloud.com>
2025-02-05 17:26:24 -0600

    CORS

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,8 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', 'true'); 
   }
 
+  res.header('Access-Control-Allow-Origin', 'https://www.ark-sri.com');  
+  res.header('Access-Control-Allow-Credentials', 'true'); 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
