049f1c7efad7a02947649df12974e098070ef66e
Tuckett45 <tucausti@icloud.com>
2025-02-06 09:31:01 -0600

    latest for proxy server requests

--- src/app/services/geocoding.service.ts
+++ src/app/services/geocoding.service.ts
@@ -9,18 +9,18 @@ export class GeocodingService {
   private proxyUrl: string;
 
   constructor(private http: HttpClient) {
-    this.proxyUrl = 'https://proxy-server-c4andmbdb5fpaqht.centralus-01.azurewebsites.net/proxy';
+    this.proxyUrl = 'https://proxy-server-c4andmbdb5fpaqht.centralus-01.azurewebsites.net';
     //'http://localhost:5000/proxy'; --LOCAL PROXY SERVER
     //'https://proxy-server-c4andmbdb5fpaqht.centralus-01.azurewebsites.net/proxy'; -- PROD PROXY SERVER
   }
 
   geocodeAddress(query: string): Observable<any> {
-    const url = `${this.proxyUrl}/geocode?query=${query}`;
+    const url = `${this.proxyUrl}/api/geocode?query=${query}`;
     return this.http.get<any[]>(url);
   }
 
   reverseGeocode(latitude: number, longitude: number): Observable<any> {
-    const url = `${this.proxyUrl}/reverse-geocode?lat=${latitude}&lon=${longitude}`;
+    const url = `${this.proxyUrl}/proxy/reverse-geocode?lat=${latitude}&lon=${longitude}`;
     return this.http.get<any>(url);
   }
 }
