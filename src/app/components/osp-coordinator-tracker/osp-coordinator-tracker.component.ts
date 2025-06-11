3d4cfe9d64e134b59053bc2e08b18e25bc214ce5
Tuckett45 <57233584+Tuckett45@users.noreply.github.com>
2025-06-11 12:44:14 -0500

    feat: allow multiple failed reasons

--- src/app/components/osp-coordinator-tracker/osp-coordinator-tracker.component.ts
+++ src/app/components/osp-coordinator-tracker/osp-coordinator-tracker.component.ts
@@ -108,7 +108,7 @@ export class OspCoordinatorTrackerComponent implements OnInit {
   
     this.dataSource.filterPredicate = (data: any, filter: string) => {
       const transformedFilter = filter.trim().toLowerCase();
-      const dataStr = `${data.segmentId} ${data.vendor} ${data.crew} ${data.materialOrder} ${data.date} ${data.workPackageCreated} ${data.amount} ${data.workPackageAmount} ${data.crew} ${data.materialOrder} ${data.originalContinuingCost} ${data.highCostAnalysis} ${data.ntp} ${data.asbuiltSubmitted} ${data.coordinatorCloseout} ${data.amendmentVersion} ${data.amendmentAmount} ${data.continuingAmount} ${data.amendmentReason} ${data.adminAudit} ${data.adminAuditDate} ${data.pass ? 'Pass' : 'Fall'} ${data.passFailReason}`.toLowerCase();
+      const dataStr = `${data.segmentId} ${data.vendor} ${data.crew} ${data.materialOrder} ${data.date} ${data.workPackageCreated} ${data.amount} ${data.workPackageAmount} ${data.crew} ${data.materialOrder} ${data.originalContinuingCost} ${data.highCostAnalysis} ${data.ntp} ${data.asbuiltSubmitted} ${data.coordinatorCloseout} ${data.amendmentVersion} ${data.amendmentAmount} ${data.continuingAmount} ${data.amendmentReason} ${data.adminAudit} ${data.adminAuditDate} ${data.pass ? 'Pass' : 'Fall'} ${(data.passFailReason || []).join(' ')}`.toLowerCase();
       return dataStr.includes(transformedFilter);
     };
   
@@ -179,7 +179,7 @@ export class OspCoordinatorTrackerComponent implements OnInit {
               adminAudit: row['adminAudit'] ? Number(row['adminAudit']) : undefined,
               adminAuditDate: row['adminAuditDate'] || '',
               pass: row['pass'] ? row['pass'].toString().toLowerCase() === 'true' : true,
-              passFailReason: row['passFailReason'] || ''
+              passFailReason: row['passFailReason'] ? row['passFailReason'].split(';').map((s: string) => s.trim()).filter((s: string) => s) : []
             };
             this.coordinatorService.addEntry(entry).subscribe();
           });
