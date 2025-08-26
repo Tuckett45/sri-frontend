c3d042ff707270331749cd8875ab37050e3cf025
Tuckett45 <tucausti@icloud.com>
2025-08-26 08:45:26 -0500

    local environment change

--- src/app/services/expense-api.service.ts
+++ src/app/services/expense-api.service.ts
@@ -15,7 +15,7 @@ export class ExpenseApiService {
     })
   };
 
-  private baseUrl = `${local_environment.apiUrl}/expenses`;
+  private baseUrl = `${environment.apiUrl}/expenses`;
 
   constructor(private http: HttpClient) {}
 
