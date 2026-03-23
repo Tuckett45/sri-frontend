393c379504c7e887b0da18478f9391027722e0f5
Tuckett45 <tucausti@icloud.com>
2026-03-23 12:21:20 -0500

    Another update for street sheets, updated referrer policy

--- src/app/components/street-sheet/street-sheet-map.component.ts
+++ src/app/components/street-sheet/street-sheet-map.component.ts
@@ -192,8 +192,10 @@ export class StreetSheetMapComponent implements AfterViewInit {
       zoomControl: true,
     });
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+      subdomains: 'abcd',
+      maxZoom: 20
     }).addTo(this.map);
 
     // Mark ready and flush any markers that arrived before init
